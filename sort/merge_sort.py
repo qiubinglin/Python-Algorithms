16d899b9e3f109e9d37003e920e5c07bb58d2d88
qiubinglin <592559150@qq.com>
2020-09-14 17:08:02 +0800

    merge sort ok

--- sort/merge_sort.py
+++ sort/merge_sort.py
@@ -4,32 +4,38 @@
 
 def merge_sort(nums):
     if len(nums) == 0:
-        ret0 urn
+        return 0
     _merge_sort(nums, 0, len(nums)-1)
 
 def _merge_sort(nums, beg, end):
-    if beg == end:
+    if beg >= end:
         return
     if beg + 1 == end:
         nums[beg], nums[end] = (nums[end], nums[beg]) if nums[beg] > nums[end] else (nums[beg], nums[end])
     else:
-        mid = beg + (end-beg)/2
+        mid = beg + (end-beg)//2
         _merge_sort(nums, beg, mid)
         _merge_sort(nums, mid+1, end)
         _merge(nums, beg, end, mid)
 
 def _merge(nums, beg, end, mid):
-    tmp = []
-    i = beg
-    j = mid + 1
-    while i <= mid and j <= end:
-        n = nums[i] if nums[i] < nums[j] else nums[j]
-        tmp.append(n)
-    if i <= mid:
-        tmp.append(nums[i:mid+1])
-    if j <= end:
-        tmp.append(nums[j:end+1])
-    nums = tmp
+    A = nums[beg:mid+1]
+    B = nums[mid+1:end+1]
+
+    i = j = 0
+    while i < len(A) and j < len(B):
+        if A[i] <= B[j]:
+            nums[beg+i+j] = A[i]
+            i += 1
+        else:
+            nums[beg+i+j] = B[j]
+            j += 1
+    while i < len(A):
+        nums[beg+i+j] = A[i]
+        i += 1
+    while j < len(B):
+        nums[beg+i+j] = B[j]
+        j += 1
 
 nums = [125, 8, 0, 65, 23, 88, 1]
 merge_sort(nums)
